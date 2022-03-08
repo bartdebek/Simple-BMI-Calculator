666ad83f216bb8537c5b21e5962990eab39163d9
Bartosz Dębek <68703788+debus3@users.noreply.github.com>
2022-03-08 22:02:20 +0100

    Update BMI calculator 2.py

--- BMI calculator 2.py
+++ BMI calculator 2.py
@@ -37,4 +37,4 @@ elif z <24.9 and z >= 18.5:
     print("\n\nYour weight is ok, congratulations!")
 
 else:
-    print("\n\nYou are underweight, try gaining some kilograms!\n\nFor healthy weight gain you should eat about {} kcal a day" .format(kcal2))
+    print("\n\nYou are underweight, try gaining some kilograms!\n\nFor healthy weight gain you should eat about {} kcal a day" .format(kcal1))
