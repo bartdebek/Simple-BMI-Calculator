896227db90ad905b75f10e7154d137dc25911442
Bartosz Dębek <68703788+debus3@users.noreply.github.com>
2022-03-11 20:24:04 +0100

    Update BMI calculator 2.py

--- BMI calculator 2.py
+++ BMI calculator 2.py
@@ -4,10 +4,10 @@ import numpy as np
 
 #Survey about individuals parameters
 
-gender = input('What is your gender?(M/F) ')
-age = input('How old are you?')
-height = input('What is your height?(in cm) ')
-weight = input('What is your weight?(in kg) ')
+gender = input("What is your gender?(M/F) ")
+age = input("How old are you? ")
+height = input("What is your height?(in cm) ")
+weight = input("What is your weight?(in kg) ")
 
 #Defining variables
 
@@ -23,7 +23,7 @@ kgpday = 300/7000            #Weight loss/gain with 300 calories deficit/surplus
 
 #Defining correct kcal intake based on Harris-Benedict equatation
 
-if gender == 'M':
+if gender == "M":
     kcal = round((1.3 * (66 + (13.7 * x) + (5  * y1) - (6.76 * a))) - 300,0)
 else:
     kcal = round((1.3 * (655 + (9.6 * x) + (1.8 * y1) - (4.7 * a))) - 300,0)
@@ -31,41 +31,42 @@ else:
 
 #Results 
 
-print('\nYour BMI is {}' .format(z))
-print('Correct BMI should be between 18.5 and 24.9')
-print('Correct weight should be between {}kg and {}kg'.format(cw1,cw2))
+print("\nYour BMI is {}" .format(z))
+print("Correct BMI should be between 18.5 and 24.9")
+print("Correct weight should be between {}kg and {}kg".format(cw1,cw2))
 
 if z > 24.9:
-    print('\nYou are a little bit overweight, try healthier diet and excercising!\n\nFor healthy weight reduction you should start eating about {} kcal a day' .format(kcal))
+    print("\nYou are a little bit overweight, try healthier diet and excercising!\n\nFor healthy weight reduction you should start eating about {} kcal a day" .format(kcal))
           
 elif z <24.9 and z >= 18.5:
-    print('\nYour weight is ok, congratulations!')
+    print("\nYour weight is ok, congratulations!")
 
 else:
-    print('\nYou are underweight, try gaining some kilograms!\n\nFor healthy weight gain you should start eating about {} kcal a day' .format(kcal))
+    print("\nYou are underweight, try gaining some kilograms!\n\nFor healthy weight gain you should start eating about {} kcal a day" .format(kcal))
 
 
 if z > 24.9: 
     nd = x - cw2 #number of kg over correct weight
     rng = round(nd / kgpday) #number of days needed to accomplish correct weight
-    o1 = np.arange(0, rng, 1) #definition of 'days' axis
-    o2 = x - o1 * 0.043 #definition of 'weight' axis
-    print('Following this rule you will reach correct weight in about {} days.'.format(rng))
+    o1 = np.arange(0, rng, 1) #definition of "days" axis
+    o2 = x - o1 * 0.043 #definition of "weight" axis
+    print("Following this rule you will reach correct weight in about {} days.".format(rng))
 
 elif z < 18.5: 
     ndw = cw1 - x #number of kg under correct weight
     rng = round(ndw / kgpday) #number of days needed to accomplish correct weight
-    o1 = np.arange(0, rng, 1) #definition of 'days' axis
-    o2 = x + o1 * 0.043 #definition of 'weight' axis
-    print('Following this rule you will reach correct weight in about {} days.'.format(rng))
+    o1 = np.arange(0, rng, 1) #definition of "days" axis
+    o2 = x + o1 * 0.043 #definition of "weight" axis
+    print("Following this rule you will reach correct weight in about {} days.".format(rng))
    
-else: sys.exit('Koniec')
+else: sys.exit("The End")
 
-print('\nGraph showing this path in simplified way:')
+print("\nGraph showing this path in simplified way:")
 # plotting the points
 plt.plot(o1, o2)
-plt.xlabel('Days')
-plt.ylabel('Weight')
+plt.xlabel("Days")
+plt.ylabel("Weight")
  
 # function to show the plot
 plt.show()
+ 
