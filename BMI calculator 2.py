9ab074f2356b159e8c95ba9c36baeb944350aca0
Bartosz Dębek <68703788+debus3@users.noreply.github.com>
2022-03-24 22:56:12 +0100

    Update BMI calculator 2.py

--- BMI calculator 2.py
+++ BMI calculator 2.py
@@ -23,9 +23,6 @@ def kcal_calc(weight,height,age,gender):
         kcal = round((1.3 * (655 + (9.6 * weight) + (1.8 * height) - (4.7 * age))) - 300,0)
     return kcal
 
-kcal = kcal_calc(weight,height,age,gender)
-
-
 #Results 
 
 print("\nYour BMI is {}" .format(roundedBMI))
